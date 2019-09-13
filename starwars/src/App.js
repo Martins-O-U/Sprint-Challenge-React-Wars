c7f61cfba2891fc0edb50839cba0f4f5abc536e2
Martins-O-U <martinsonyedikachi@gmail.com>
2019-09-13 11:16:21 +0100

    Self study questions answered, Data fetched and displayed on the broswer

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,19 +1,48 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
+
+import Person from './Person';
+// const url = 'https://swapi.co/api/people/?format=json';
+
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+	// Try to think through what state you'll need for this app before starting. Then build out
+	// the state properties here.
+	const [people, setPeople] = useState([])
+	// Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
+	// side effect in a component, you want to think about which state and/or props it should
+	// sync up with, if any.
+	useEffect(() => {
+		axios.get(`https://swapi.co/api/people`)
+		.then(response => {
+			console.log(response);
+			setPeople(response.data.results);
+		})
+		.catch(error => console.error(error))
+		return () => {
+			console.log('cleanup')
+		};
+	}, [])
+	return (
+		<div className="App">
+			<h1 className="Header">React Wars</h1>
+      <table>
+      <tr>
+    <th>Name</th>
+    <th>Height</th>
+    <th>Hair Color</th>
+    <th>Skin Color</th>
+    <th>Birth Year</th>
+    <th>Gender</th>
+  </tr>
+      </table>
 
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
-
-  return (
-    <div className="App">
-      <h1 className="Header">React Wars</h1>
-    </div>
-  );
+			{
+				people ? people.map(person => <Person data={person} key={person.url} />) : null
+			}
+		</div>
+	);
 }
 
 export default App;
